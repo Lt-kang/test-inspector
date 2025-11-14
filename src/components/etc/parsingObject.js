7d5fe30029862bedea5b8547564739d069e19aa7
Seung Hyun <k_penrose@naver.com>
2025-11-14 15:02:02 +0900

    디버깅 / 특정 수학 latex 문장에서 오류가 나던 부분 수정

--- src/components/etc/parsingObject.js
+++ src/components/etc/parsingObject.js
@@ -1,12 +1,33 @@
-function ParsingChoice(choice) {
-    let text = ""
-  
-    for (const key in choice) {
-      text += `<h3 style="white-space: nowrap; overflow-wrap: normal; word-break: normal; display: inline;">${key}</h3>: ${choice[key]}<br>`
-    }
-    
-    return text
-  }
+// function ParsingChoice(choice) {
+//     let text = ""
 
+//     const escapeHtml = (unsafe) => {
+//       return unsafe
+//         .replace(/&/g, "&amp;")
+//         .replace(/</g, "&lt;")
+//         .replace(/>/g, "&gt;")
+//         .replace(/"/g, "&quot;")
+//         .replace(/'/g, "&#039;");
+//     }
+  
+//     for (const key in choice) {
+//       text += `<h3 style="white-space: nowrap; overflow-wrap: normal; word-break: normal; display: inline;">${key}</h3>: ${choice[key]}<br>`
+//     }
+    
+//     return text
+//   }
+
+
+// export { ParsingChoice }
+
+function ParsingChoice(choice) {
+  let text = ""
+
+  for (const key in choice) {
+    text += `**${key}**: ${choice[key]}\n\n`
+  }
+  
+  return text
+}
 
 export { ParsingChoice }
